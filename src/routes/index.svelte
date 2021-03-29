9edf7f13b4c9fbd05ba0e170b57d6351a99f4361
arperyan <ryan_arpe@hotmail.com>
2021-03-29 10:39:39 +0100

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,19 @@
 <script context="module">
     export const prerender = true;
     export function load() {
-        return { redirect: "/products/1" };
+        return { redirect: "/products/1", status: 301 };
     }
 </script>
+
+<!-- <script>
+    import { onMount } from "svelte";
+
+    let awkward;
+
+    onMount(async () => {
+        const module = await import("$lib/styles/global");
+        awkward = module;
+    });
+
+    awkward;
+</script> -->
