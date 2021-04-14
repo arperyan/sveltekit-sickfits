bb7c355f11d935d58a11e1436314db9cddfcc696
Ryan Arpe <ryan_arpe@hotmail.com>
2021-04-14 21:51:06 +0100

    Create $error.svelte

---
+++ src/routes/$error.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  export async function load({ error, status }) {
+    return {
+      props: { error, status }
+    };
+  }
+</script>
+<script>
+  export let status;
+  export let error;
+</script>
+<h1>{status}</h1>
+<p>{error.message}</p>
+{#if error.stack}
+  <pre>{error.stack}</pre>
+{/if}
