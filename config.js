e856df32dba007675a0458bb92732a22d5deeeb8
arperyan <ryan_arpe@hotmail.com>
2021-07-28 15:13:51 +0100

    update to config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://4b01ca8c898e.ngrok.io/api/graphql`;
+export const prodEndpoint = `http://522838ce98df.ngrok.io/api/graphql`;
 export const perPage = 4;
